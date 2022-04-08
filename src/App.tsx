3cb0655f335b640b0610e0e0c07469e9f2f356ce
TJ Phan <t.jamesphan@gmail.com>
2022-04-08 10:43:10 -0700

    migrated props to main App component from Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import Input from './components/Input';
 
-function App() {
+const App: React.FC = () => {
+  const [ value, setValue ] = useState<string>("")
+  const [ deck, setDeck ] = useState([])
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (value) {
+      console.log(e);
+    }
+  }
+
   return (
     <div className="App">
-      <Input />
+      <Input 
+        value={value} 
+        setValue={setValue}
+        handleSubmit={handleSubmit}
+      />
     </div>
   );
 }

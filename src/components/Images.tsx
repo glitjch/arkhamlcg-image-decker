7c7f53fdac0399fe6ec41ccc63aec1f7e3ec6c3e
TJ Phan <t.jamesphan@gmail.com>
2022-04-11 08:53:49 -0700

    added in basic print option for individual and multiple images

--- src/components/Images.tsx
+++ src/components/Images.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import printJS from 'print-js';
+import React, { useEffect, useState } from 'react'
 import '../styles.scss'
-import SingleImage from './SingleImage'
+import SingleImage from './SingleImage';
+
 
   interface Props {
     images: string[],
@@ -10,15 +12,45 @@ import SingleImage from './SingleImage'
 
 // COMPONENT
 const Images: React.FC<Props> = ({images, setImages, generateImages}) => {
+  const [ print, setPrint ] = useState<string[]>([]);
+
+  useEffect(() => {
+    const printAll = () => {
+      const printArray: string[] = [];
+      images.filter(image => image !== undefined).map(image => printArray.push("https://arkhamdb.com" + image));
+      setPrint(printArray.slice(0, 5))
+    }
+    printAll();
   
+  }, [images])
+  
+
   // VIEW
   return (
     <div className='Images__container'>
       <form className='getImage'>
         <button type='button' onClick={()=> generateImages()}>Generate</button>
       </form>
-      {images && images.map((image) => {
-        
+      <button type="button" onClick={() => {
+        printJS({
+          printable: print,
+          type: 'image',
+          targetStyles: [ 
+          "*"
+          ],
+          header: null,
+          css: "../styles.scss",
+          style: "margin: 50"
+
+        })
+        }}>
+        Print All {print.length} Cards
+      </button>
+      {print.map(p => {
+        return <ul>{p}</ul>
+      })}
+
+      {images && images.slice(0,5).map((image) => {
         return (
           <SingleImage image={image} key={Date.now() + Math.random()}/>
         )
--- src/components/SingleImage.tsx
+++ src/components/SingleImage.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import printJS from 'print-js';
 
 interface Props {
   image: string,
@@ -10,7 +11,10 @@ const SingleImage: React.FC<Props> = ({image, key}) => {
 
   // VIEW
   return (
-     <img key={key} className='SingleImage' src={"https://arkhamdb.com/" + image} /> 
+    <div>
+      <img key={key} className='SingleImage' src={"https://arkhamdb.com/" + image} /> 
+      <button type="button" onClick={() => printJS(`https://arkhamdb.com/${image}`, "image")}>Print</button>
+    </div>
   )
 }
 export default SingleImage

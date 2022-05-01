c7863b36af3b74fc892ffe374b180942aa1fdcf9
TJ Phan <t.jamesphan@gmail.com>
2022-05-01 10:28:03 -0700

    Refactor InputFormItem renaming variables

--- src/components/InputFormItem.tsx
+++ src/components/InputFormItem.tsx
@@ -10,16 +10,22 @@ const InputFormItem = (props: any) => {
   // Refocuses on the next input slot once user inserts value. 5 slots total.
   useEffect(() => {
     empty < 6 && props.focusInput()
-  }, [empty])
+  }, [empty]);
 
   // Prevents user from retyping in the same input after value exists
+  const disableFilledInput = (index: number) => {
+    let filledInput = (document.getElementById(`digit${index}`) as HTMLInputElement);
+    filledInput.disabled = true;
+  };
+  
+  const handleInputChange = (event: any) => {
+    setValues((prev: any) => prev + event.target.value);
+    setEmpty(prev => prev + 1)
+  };
+
+  // Generates multiple slots with state conditions
   function multiplyNumberSlots () {
     const slotsArray:any = [];
-    function disableFilledInput (index: number){
-      let filledInput = (document.getElementById(`digit${index}`) as HTMLInputElement);
-      filledInput.disabled = true;
-    }
-
     for (let i = 1; i <= 5; i++) {
       slotsArray.push(
         <input 
@@ -30,10 +36,8 @@ const InputFormItem = (props: any) => {
           id={`digit${i}`}
           key={i}
           onChange={(e) => {
-            const singleInput = e.target.value;
-            setValues((prev: any) => prev + singleInput);
+            handleInputChange(e)
             disableFilledInput(i);
-            setEmpty(prev => prev + 1)
           }} 
         />
       )
@@ -50,6 +54,6 @@ const InputFormItem = (props: any) => {
       {multiplyNumberSlots()}
     </form>
   )
-}
+};
 
-export default InputFormItem
+export default InputFormItem;

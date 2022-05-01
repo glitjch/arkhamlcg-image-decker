77d9a15aef245fc4303670b78a402b30be0f47bb
TJ Phan <t.jamesphan@gmail.com>
2022-04-30 23:11:57 -0700

    Add image

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -9,7 +9,16 @@ const Instructions = (props: any) => {
   return (
     <aside 
       className='Instructions__container'>
-      Instructions
+      <h4>
+        Less Writing, More Campaigning!
+      </h4>
+      <p>Print out your proxies here:</p>
+      <ol>
+        <li>Click Begin Ritual</li>
+        <li>Enter your Arkham LCG Deck ID</li>
+        <li>Click Generate</li>
+        <li>Print Cards</li>
+      </ol>
     </aside>
   )
 }
